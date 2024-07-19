43642cde2a3fd9b0245999f860949b275dfcc918
Nikhil Singh <101012986+Nikhilsingh2003@users.noreply.github.com>
2024-07-19 15:06:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
 import { Helmet } from 'react-helmet';
 import './App.css';
 import ResumeState from './Context/ResumeState';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Navbar from './Components/Navbar/Navbar';
+import About from './Pages/About/About';
 
 function App() {
   return (
